0c8e747261811d810f3bfbcbeb07d50922896c41
Ebooth <pauldejeandev@gmail.com>
2025-02-20 16:45:53 +0100

    [Step 2] Added option to count lines

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::fs;
 
 #[derive(Parser, Debug)]
 #[command(name = "cwc")]
@@ -9,15 +10,29 @@ pub struct Args {
     #[arg(short = 'c', group = "byte_count")]
     pub bytes: bool,
 
+    #[arg(short = 'l', group = "line_count")]
+    pub lines: bool,
+
     pub file_paths: Vec<String>,
 }
 
 pub fn execute_command(args: Args) -> i32 {
-    println!("args: {}", args.file_paths.join(" "));
+    // println!("args: {}", args.file_paths.join(" "));
     let file_paths = args.file_paths;
     if file_paths.is_empty() {
         print_error("Missing files argument");
         return 1;
+    }
+
+    let flags = vec![args.bytes, args.lines];
+    let count = flags.into_iter().filter(|&flag| flag).count();
+    if count > 1 {
+        print_error("Only one of -c (bytes) or -l (lines) can be provided.");
+        std::process::exit(1);
+    }
+    if count == 0 {
+        print_error("No flag provided. Please provide either -c (bytes) or -l (lines).");
+        std::process::exit(1);
     }
 
     let mut error_code = 0;
@@ -38,22 +53,30 @@ pub fn execute_command(args: Args) -> i32 {
                 continue;
             }
         };
-        let bytes = file_content.len();
-        total += bytes;
-        println!("{:>8} {}", bytes, path);
+
+        if args.bytes {
+            let bytes = file_content.len();
+            total += bytes;
+            println!("{:>8} {}", bytes, path);
+        } else if args.lines {
+            let lines_number = file_content.matches("\n").count();
+            total += lines_number;
+            println!("{:>8} {}", lines_number, path);
+        }
     }
     if file_paths.len() > 1 {
         println!("{:>8} total", total);
     }
     return error_code;
 }
+
 fn is_file_path_valid(file_path: &str) -> bool {
     let path = std::path::Path::new(file_path);
     return path.exists() && path.is_file();
 }
 
 fn read_file(file_path: &str) -> std::io::Result<String> {
-    std::fs::read_to_string(file_path)
+    fs::read_to_string(file_path)
 }
 
 fn print_error(error: &str) {
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -15,10 +15,32 @@ fn create_temp_file(content: &str) -> NamedTempFile {
 #[test]
 fn test_no_file_argument() {
     let mut cmd = Command::cargo_bin("cwc-tool").unwrap();
-
-    cmd.assert()
+    cmd.arg("-c")
+        .assert()
         .code(1)
         .stderr(predicate::str::contains("cwc: Missing files argument"));
+}
+
+#[test]
+fn test_no_flag_argument() {
+    let mut cmd = Command::cargo_bin("cwc-tool").unwrap();
+    cmd.arg("filename.txt")
+        .assert()
+        .code(1)
+        .stderr(predicate::str::contains(
+            "cwc: No flag provided. Please provide either -c (bytes) or -l (lines).",
+        ));
+}
+#[test]
+fn test_multiple_flags_argument() {
+    let mut cmd = Command::cargo_bin("cwc-tool").unwrap();
+    cmd.arg("filename.txt")
+        .args(&["-c", "-l"])
+        .assert()
+        .code(1)
+        .stderr(predicate::str::contains(
+            "cwc: Only one of -c (bytes) or -l (lines) can be provided",
+        ));
 }
 
 #[test]
@@ -26,7 +48,8 @@ fn test_file_non_existent() {
     let mut cmd = Command::cargo_bin("cwc-tool").unwrap();
     let filename = "non_existent_file.txt";
 
-    cmd.arg(filename)
+    cmd.arg("-c")
+        .arg(filename)
         .assert()
         .code(1)
         .stderr(predicate::str::contains(format!(
@@ -36,13 +59,14 @@ fn test_file_non_existent() {
 }
 
 #[test]
-fn test_one_file() {
+fn test_count_bytes_one_file() {
     let file_content = "Hello, world!";
     let mut cmd = Command::cargo_bin("cwc-tool").unwrap();
     let file = create_temp_file(file_content);
     let file_path = file.path().to_str().unwrap();
 
-    cmd.arg(file_path)
+    cmd.arg("-c")
+        .arg(file_path)
         .assert()
         .code(0)
         .stdout(predicate::str::contains(&format!(
@@ -52,7 +76,7 @@ fn test_one_file() {
 }
 
 #[test]
-fn test_two_files() {
+fn test_count_bytes_two_files() {
     let file_content1 = "Hello, world!";
     let file1 = create_temp_file(file_content1);
     let file_path1 = file1.path().to_str().unwrap();
@@ -70,7 +94,49 @@ fn test_two_files() {
         predicate::str::contains(&format!("{:>8} {}", 209, file_path2))
             .and(predicate::str::contains(&format!("{:>8} {}", 222, "total"))),
     );
-    cmd.args(&[file_path1, file_path2])
+    cmd.arg("-c")
+        .args(&[file_path1, file_path2])
+        .assert()
+        .code(0)
+        .stdout(stdout_predicate);
+}
+
+#[test]
+fn test_count_lines_one_file() {
+    let file_content = "Hello,
+    world!";
+    let mut cmd = Command::cargo_bin("cwc-tool").unwrap();
+    let file = create_temp_file(file_content);
+    let file_path = file.path().to_str().unwrap();
+
+    cmd.arg("-l")
+        .arg(file_path)
+        .assert()
+        .code(0)
+        .stdout(predicate::str::contains(&format!("{:>8} {}", 1, file_path)));
+}
+
+#[test]
+fn test_count_lines_two_files() {
+    let file_content1 = "Hello, world!";
+    let file1 = create_temp_file(file_content1);
+    let file_path1 = file1.path().to_str().unwrap();
+
+    let file_content2 = "I have a dream!
+    I have a dream that one day on the red hills of Georgia,
+    the sons of former slaves and the sons of former slave owners
+    will be able to sit down together at the table of brotherhood.";
+    let file2 = create_temp_file(file_content2);
+    let file_path2 = file2.path().to_str().unwrap();
+
+    let mut cmd = Command::cargo_bin("cwc-tool").unwrap();
+
+    let stdout_predicate = predicate::str::contains(&format!("{:>8} {}", 0, file_path1)).and(
+        predicate::str::contains(&format!("{:>8} {}", 3, file_path2))
+            .and(predicate::str::contains(&format!("{:>8} {}", 3, "total"))),
+    );
+    cmd.arg("-l")
+        .args(&[file_path1, file_path2])
         .assert()
         .code(0)
         .stdout(stdout_predicate);
